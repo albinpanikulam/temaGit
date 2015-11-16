ca510d36b7b72811a73b850895fab2375be5e54f
albin <albinpanikulam@gmail.com>
2015-11-16 15:05:04 +0100

    modificado: Uno.java

--- Uno.java
+++ Uno.java
@@ -2,7 +2,7 @@ public class Uno {
 	
 	public static void main (String args[]) {
 		System.out.println("Este esa una fase de desarrollo");
-		
+		System.out.println("Añadiendo una nueva linea en master");
 	}
 }
 
